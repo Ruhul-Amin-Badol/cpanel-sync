534a236ba25a608722ba479555bf1e2e328a2926
Ruhul-Amin-Badol <ruhul.gub@gmail.com>
2023-12-21 13:08:10 +0600

    sync

---
+++ sync_repo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Step 1: Download changes from cPanel
+# Use wget or curl to download files
+
+# Step 2: Update local Git repository
+git pull origin master  # or use the appropriate branch
+
+# Step 3: Commit changes locally
+git add .
+git commit -m "Sync changes from cPanel"
+
+# Step 4: Push changes to remote Git repository
+git push origin master  # or use the appropriate branch
